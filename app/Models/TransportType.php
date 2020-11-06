e03edfd9b37da35bedb21d027f2024ffeb6ca398
Royko7 <ura.royko@gmail.com>
2020-11-06 11:47:57 +0200

    add seeder CityCourseType

--- app/Models/TransportType.php
+++ app/Models/TransportType.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CityCourseType extends Model
+class TransportType extends Model
 {
     //
 }
---
+++ database/seeds/CityCourseTypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+use App\Models\CityCourseType;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CityCourseTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('city_course_types')->insert([
+            'course_type' => 'Міський',
+            'created_at' => '2020-02-22 17:50:00',
+            'updated_at' => '2020-08-29 18:55:37',
+        ]);
+        DB::table('city_course_types')->insert([
+            'course_type' => 'Між міський',
+            'created_at' => '2020-02-22 17:50:00',
+            'updated_at' => '2020-08-29 18:55:37',
+        ]);
+    }
+}
